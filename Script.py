a5399844c2b24fd0e9a57452290d057023d37fae
MAJORTG <99739229+MAJORTG2@users.noreply.github.com>
2023-03-17 16:43:08 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,8 +11,8 @@ class script(object):
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
+- NOT A OPEN SOURCE PROJECT IF U NEED CONTACT OWNER @TGx
+- Source - https://github.com/MAJORTG2
 
 <b>DEVS:</b>
 - <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
